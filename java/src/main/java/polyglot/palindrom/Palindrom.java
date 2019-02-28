dc15bacb5ceb0ed600f033d2a6887c37fb196aa9
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2019-02-28 08:35:40 +0100

    Implemented reverse palindrom.

--- java/src/main/java/polyglot/palindrom/Palindrom.java
+++ java/src/main/java/polyglot/palindrom/Palindrom.java
@@ -1,18 +1,26 @@
 package polyglot.palindrom;
 
-import java.util.Objects;
+import static java.util.Objects.isNull;
 
 public class Palindrom {
 
-    public boolean isPalndrom(String value) {
-        if (Objects.isNull(value)) {
+    public boolean isPalindrom(String value) {
+        if (isNull(value)) {
             return false;
+        } else if (value.length() <= 1) {
+            return true;
         }
 
         return true;
     }
 
-    public boolean someLibraryMethod() {
-        return true;
+    public boolean isPalindromReverse(String value) {
+        if (isNull(value)) {
+            return false;
+        }
+
+        StringBuilder b = new StringBuilder(value);
+        return value.equals(b.reverse().toString());
     }
+
 }
