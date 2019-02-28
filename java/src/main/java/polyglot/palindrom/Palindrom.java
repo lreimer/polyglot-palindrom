44df5811e9cdef0eb9ca79110055534bccb058b6
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2019-02-28 10:46:10 +0100

    Implemented iterative and recursive Palindrom.

--- java/src/main/java/polyglot/palindrom/Palindrom.java
+++ java/src/main/java/polyglot/palindrom/Palindrom.java
@@ -11,7 +11,27 @@ public class Palindrom {
             return true;
         }
 
+        char[] chars = value.toCharArray();
+        for (int i = 0; i < chars.length / 2; i++) {
+            if (chars[i] != chars[chars.length - i - 1]) {
+                return false;
+            }
+        }
+
         return true;
+    }
+
+    public boolean isPalindromRecursive(String value) {
+        if (isNull(value)) {
+            return false;
+        } else if (value.length() <= 1) {
+            return true;
+        }
+
+        char first = value.charAt(0);
+        char last = value.charAt(value.length() - 1);
+
+        return (first == last) && isPalindromRecursive(value.substring(1, value.length() - 1));
     }
 
     public boolean isPalindromReverse(String value) {
--- java/src/test/groovy/polyglot/palindrom/PalindromSpec.groovy
+++ java/src/test/groovy/polyglot/palindrom/PalindromSpec.groovy
@@ -1,5 +1,6 @@
 package polyglot.palindrom
 
+import spock.lang.Shared
 import spock.lang.Specification
 import spock.lang.Subject
 import spock.lang.Title
@@ -7,11 +8,37 @@ import spock.lang.Title
 @Title('Test spec for the Java Palindrom implementation')
 class PalindromSpec extends Specification {
 
+    @Shared
     @Subject
     Palindrom palindrom
 
-    void setup() {
+    @Shared
+    def values
+    @Shared
+    def results
+
+    void setupSpec() {
         palindrom = new Palindrom()
+        values = [null, "", "a", "ab", "aba", "abba", "abcba", "0123456789876543210", "1qaz2wsx3edc4rfv"]
+        results = [false, true, true, false, true, true, true, true, false]
+    }
+
+    def "Check default Palindrom implementation"() {
+        expect:
+        palindrom.isPalindrom(value) == result
+
+        where:
+        value << values
+        result << results
+    }
+
+    def "Check recursive Palindrom implementation"() {
+        expect:
+        palindrom.isPalindromRecursive(value) == result
+
+        where:
+        value << values
+        result << results
     }
 
     def "Check reverse Palindrom implementation"() {
@@ -19,14 +46,7 @@ class PalindromSpec extends Specification {
         palindrom.isPalindromReverse(value) == result
 
         where:
-        value                 | result
-        null                  | false
-        "1qaz2wsx3edc4rfv"    | false
-        ""                    | true
-        "a"                   | true
-        "aba"                 | true
-        "abba"                | true
-        "abcba"               | true
-        "0123456789876543210" | true
+        value << values
+        result << results
     }
 }
